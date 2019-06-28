a638d53fa2719fcaaad325dbec6dafaa9e452290
cloud <lantianyun@wangankeji.cn>
2019-06-28 14:22:30 +0800

    详细 bug message: "too many values to unpack (expected 2)"

--- app/db/es.py
+++ app/db/es.py
@@ -468,7 +468,7 @@ class ESClient(object):
     @addHead()
     def query_yth_base_by_indexid(self, params):
         index_id = params['index_id']
-        return self.es.get(index='yth_base', doc_type='mytype', id=index_id)
+        return True,self.es.get(index='yth_base', doc_type='mytype', id=index_id)
 
 
     # -------------------------加入告警到alarm——list-------------------------------------------
