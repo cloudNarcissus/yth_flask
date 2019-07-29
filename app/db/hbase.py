dda146ccc8788e82dceb23ef11117df457fab19b
cloud <lantianyun@wangankeji.cn>
2019-07-29 09:13:42 +0800

    调整hbase连接池为单hbase连接

--- app/db/hbase.py
+++ app/db/hbase.py
@@ -19,14 +19,11 @@ class HbaseConnect(object):
             self.log.error('hb_pool init failed,%s' % str(e))
 
     def get_conn(self):
-        if self.hb_pool is not None:
-            conn = hbc.hb_pool.connection()
-        else:
-            try:
-                conn = happybase.Connection(host=Config.hb_hosts, port=Config.hb_port, autoconnect=True)
-            except Exception as e:
-                conn = None
-                self.log.error('happybase.Connection failed,%s' % str(e))
+        try:
+            conn = happybase.Connection(host=Config.hb_hosts, port=Config.hb_port, autoconnect=True)
+        except Exception as e:
+            conn = None
+            self.log.error('happybase.Connection failed,%s' % str(e))
         return conn
 
     def dowmload_wdp_files(self, params):
@@ -38,6 +35,7 @@ class HbaseConnect(object):
         md5 = params.get('__md5')
         content = b''
 
+        conn = None
         try:
             with self.get_conn() as conn:
                 files = conn.table(b'files')
@@ -52,7 +50,7 @@ class HbaseConnect(object):
                     content += row[column_name]
 
         except Exception as e:
-            self.log.debug(u'获取文件<Md5:%s>失败,因conn异常:%s', md5, e)
+            self.log.error(u'获取文件<Md5:%s>失败,因conn异常:%s', md5, e)
             return False,str(e)
         finally:
             if conn is not None:
